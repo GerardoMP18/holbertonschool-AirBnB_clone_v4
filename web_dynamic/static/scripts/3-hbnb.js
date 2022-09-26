8ad60b04fe4712f59ecfe1c20a0e6a816cbc5588
gerardomp18 <gerardomarinparra18@gmail.com>
2022-09-26 02:24:07 -0500

    [fix] correction of semistander

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -22,28 +22,28 @@ $(document).ready(() => {
     url: 'http://127.0.0.1:5001/api/v1/places_search/',
     contentType: 'application/json',
     data: JSON.stringify({})
-  }).done((data)  => {
+  }).done((data) => {
     for (const place of data) {
       let template = '';
       const plural = (element) => {
-        return element != 1 ? 's' : '';
-      }
+        return element !== 1 ? 's' : '';
+      };
       template += '<article>';
       template += '  <div class="title_box">';
       template += '    <h2>' + place.name + '</h2>';
-      template += '    <div class="price_by_night">' + place.price_by_night + '</div>'
+      template += '    <div class="price_by_night">' + '$' + place.price_by_night + '</div>';
       template += '  </div>';
       template += '  <div class="information">';
       template += '    <div class="max_guest">' + place.max_guest + ' Guest' + plural(place.max_guest) + '</div>';
       template += '    <div class="number_rooms">' + place.number_rooms + ' Bedroom' + plural(place.number_rooms) + '</div>';
       template += '    <div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + plural(place.number_bathrooms) + '</div>';
       template += '  </div>';
-      template += '    <div class="user">'
-      template += '    </div>'
+      template += '    <div class="user">';
+      template += '    </div>';
       template += '    <div class="description">' + place.description + '</div>';
       template += '  </div>';
       template += '</article>';
-    $('section.places').append(template);
+      $('section.places').append(template);
     }
   });
 });
