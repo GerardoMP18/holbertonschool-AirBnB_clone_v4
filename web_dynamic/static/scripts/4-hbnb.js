78cb00f292372b31f92eca393911f0b2593ef0ac
gerardomp18 <gerardomarinparra18@gmail.com>
2022-09-26 02:24:56 -0500

    [fix] Adding script trigger for the view in menu the places in event click false

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -16,38 +16,39 @@ $(document).ready(() => {
       $('#api_status').removeClass('available');
     }
   });
-  $('.filters button').click(() => { 
-    // console.log(Object.keys(newAmenity));
+
+  const plural = (element) => {
+    return element !== 1 ? 's' : '';
+  };
+  $('.filters button').click(() => {
     $.ajax({
       type: 'POST',
       url: 'http://127.0.0.1:5001/api/v1/places_search/',
       contentType: 'application/json',
-      data: JSON.stringify({ amenities : newAmenity })
-    }).done((data)  => {
+      data: JSON.stringify({ amenities: Object.keys(newAmenity) })
+    }).done((data) => {
       $('section.places').empty();
       for (const place of data) {
         let template = '';
-        const plural = (element) => {
-          return element != 1 ? 's' : '';
-        }
         template += '<article>';
         template += '  <div class="title_box">';
         template += '    <h2>' + place.name + '</h2>';
-        template += '    <div class="price_by_night">' + '$' + place.price_by_night + '</div>'
+        template += '    <div class="price_by_night">' + '$' + place.price_by_night + '</div>';
         template += '  </div>';
         template += '  <div class="information">';
         template += '    <div class="max_guest">' + place.max_guest + ' Guest' + plural(place.max_guest) + '</div>';
         template += '    <div class="number_rooms">' + place.number_rooms + ' Bedroom' + plural(place.number_rooms) + '</div>';
         template += '    <div class="number_bathrooms">' + place.number_bathrooms + ' Bathroom' + plural(place.number_bathrooms) + '</div>';
         template += '  </div>';
-        template += '    <div class="user">'
-        template += '    </div>'
+        template += '    <div class="user">';
+        template += '    </div>';
         template += '    <div class="description">' + place.description + '</div>';
         template += '  </div>';
         template += '</article>';
-      $('section.places').append(template);
+        $('section.places').append(template);
       }
     });
   });
-  // Envi­a una solicitud POST con Content-Type: application/json
+  // Agregando triger para que pueda ser mostrado los places al ser cargado el DOM
+  $('.filters button').trigger('click');
 });
